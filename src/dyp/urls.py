af054c5abdda3c263eb02ac7f0af3e6961ecf081
Bharathi <bharathiranganathan24@gmail.com>
2016-07-08 14:05:21 -0700

    Creating a new page called share page for social sharing

--- src/dyp/urls.py
+++ src/dyp/urls.py
@@ -5,10 +5,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'joins.views.home', name='home'),
     url(r'^',include('favicon.urls')),
+    url(r'^(?P<ref_id>.*)$',  'joins.views.share', name='share'),
     #url(r'^home2/$', 'joins.views.home2', name='home2'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    
 )
--- src/joins/views.py
+++ src/joins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 
 # Create your views here.
 
@@ -31,22 +31,15 @@ def get_ref_id():
 	except:
 		return ref_id
 
+def share(request,ref_id):
+	print ref_id
+	context = {"ref_id" : ref_id}
+	template = "share.html"
+	return render(request,template,context)
 
 
 def home(request):
-	# print request.META.get("REMOTE_ADDR")
-	# print request.META.get("HTTP_X_FORWARDED_FOR")
-	#print request.GET["email"],request.POST["email_2"]
-	#print request.POST
-	# form = EmailForm(request.POST or None)
-	# if form.is_valid():
-	# 	email = form.cleaned_data['email']
-	# 	new_join, created = Join.objects.get_or_create(email=email)
-	# 	print new_join,created
-	# 	print new_join.timestamp
-	# 	if created:
-	# 		print "obj was created"
-
+	
 	form = JoinForm(request.POST or None)
 	if form.is_valid():
 		new_join=form.save(commit=False)
@@ -58,6 +51,7 @@ def home(request):
 			new_join_old.save()
 		#new_join.ip_address = get_ip(request)
 		#new_join.save()
+		return HttpResponseRedirect("/%s" %(new_join_old.ref_id))  
 	context = {"form": form}
 	template = "home.html"
 	return render(request,template,context)
