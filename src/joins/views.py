a13d2d9821d191dfc9e6de7e00687f48db2cb12b
Bharathi <bharathiranganathan24@gmail.com>
2016-07-08 10:56:15 -0700

    Getting User IP address

--- src/joins/views.py
+++ src/joins/views.py
@@ -5,9 +5,23 @@ from django.shortcuts import render
 from .forms import EmailForm,JoinForm
 from .models import Join
 
+def get_ip(request):
+	try:
+		x_forward = request.META.get("HTTP_X_FORWARDED_FOR")
+		if x_forward:
+			ip = x_forward.split(",")[0]
+		else:
+			ip = request.META.get('REMOTE_ADDR')
+	except:
+		ip = ""
+	return ip
+
+
 
 
 def home(request):
+	# print request.META.get("REMOTE_ADDR")
+	# print request.META.get("HTTP_X_FORWARDED_FOR")
 	#print request.GET["email"],request.POST["email_2"]
 	#print request.POST
 	# form = EmailForm(request.POST or None)
@@ -23,7 +37,11 @@ def home(request):
 	if form.is_valid():
 		new_join=form.save(commit=False)
 		email = form.cleaned_data['email']
-		new_join_old = Join.objects.filter(email=email)
+		new_join_old, created = Join.objects.get_or_create(email=email)
+		if created:
+			new_join_old.ip_address = get_ip(request)
+			new_join_old.save()
+		#new_join.ip_address = get_ip(request)
 		#new_join.save()
 	context = {"form": form}
 	template = "home.html"
