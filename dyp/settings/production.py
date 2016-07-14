602eefd1d7c2c9617f5ea163296512fe23ab14ff
Bharathi <bharathiranganathan24@gmail.com>
2016-07-13 19:01:21 -0700

    Update reference link

--- dyp/settings/production.py
+++ dyp/settings/production.py
@@ -2,7 +2,7 @@ import os
 from django.conf import settings
 
 DEBUG = False
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 DATABASES = settings.DATABASES
 
@@ -17,7 +17,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 
-#SHARE_URL = "http://ancient-castle-2299.herokuapp.com/?ref="
+SHARE_URL = "https://aqueous-garden-85296.herokuapp.com/?ref="
 # Static asset configuration
 # import os
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
